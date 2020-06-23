3e65e5aec451257cd74259c6f3917e2a9126d509
john leonhel ang <38998788+leonhelthe@users.noreply.github.com>
2020-06-23 15:47:02 +0800

    Add files via upload
    added built react files

---
+++ precache-manifest.cb593b353ff74b22a58f7e557d954bd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f06ff572e02a40a8fab574b133828e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a2b065ae90ec0d977f6",
+    "url": "/static/css/main.0101c9a6.chunk.css"
+  },
+  {
+    "revision": "846797f4a17466ba01dd",
+    "url": "/static/js/2.2bf1e89d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2bf1e89d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2b065ae90ec0d977f6",
+    "url": "/static/js/main.5f3e391a.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
